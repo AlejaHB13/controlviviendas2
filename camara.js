a8c1d808b2a82e57efdaa5e2491fe44237d9f2f7
Laura Beltran <lauras.beltranb@konradlorenz.edu.co>
2023-05-08 22:17:06 -0500

    video
    Se implementa video

---
+++ camara.js
@@ -0,0 +1,106 @@
+let video = null;
+let detector = null;
+let detections = [];
+let videoVisibility = true;
+let detecting = false;
+
+const videoAction = document.getElementById('videoAction');
+const detectionAction = document.getElementById('detectionAction');
+
+document.body.style.cursor = 'wait';
+
+function preload() {
+  detector = ml5.objectDetector('cocossd');
+}
+
+function setup() {
+  createCanvas(150, 150);
+}
+
+function draw() {
+  if (!video || !detecting) return;
+  image(video, 0, 0);
+  for (let i = 0; i < detections.length; i++) {
+    drawResult(detections[i]);
+  }
+}
+
+function drawResult(object) {
+  boundingBox(object);
+  drawLabel(object);
+}
+
+function boundingBox(object) {
+  stroke('blue');
+  strokeWeight(6);
+  noFill();
+  rect(object.x, object.y, object.width, object.height);
+}
+function drawLabel(object) {
+  noStroke();
+  fill('white');
+  textSize(34);
+  text(object.label, object.x + 15, object.y + 34);
+}
+
+function onDetected(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  detections = results;
+
+  if (detecting) {
+    detect();
+  }
+}
+
+function detect() {
+  detector.detect(video, onDetected);
+}
+
+function toggleVideo() {
+  if (!video) return;
+  if (videoVisibility) {
+    video.hide();
+    videoAction.innerText = 'Activar Video';
+  } else {
+    video.show();
+    videoAction.innerText = 'Desactivar Video';
+  }
+  videoVisibility = !videoVisibility;
+}
+
+function toggleDetecting() {
+  if (!video || !detector) return;
+  if (!detecting) {
+    detect();
+    detectionAction.innerText = 'Parar...';
+  } else {
+    detectionAction.innerText = 'Detectar Objetos';
+  }
+  detecting = !detecting;
+}
+
+function startCamera() {
+  video = createCapture(VIDEO);
+  video.size(150, 150);
+  video.hide();
+}
+
+function recognizeSpeech() {
+  const recognition = new webkitSpeechRecognition();
+  recognition.lang = "es-ES";
+
+  recognition.onresult = function(event) {
+    const command = event.results[0][0].transcript.toLowerCase();
+    if (command === "activar cámara") {
+      startCamera();
+    }
+  }
+
+  recognition.start();
+}
+
+window.onload = function() {
+  recognizeSpeech();
+}
